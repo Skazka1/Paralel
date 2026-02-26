2a79b03b4042c33a15c6233113484f7c12eee698
Skazka1 <125551509+smoozeboy@users.noreply.github.com>
2026-02-26 10:16:15 +0500

    вроде все

--- Paral/main.cpp
+++ Paral/main.cpp
@@ -8,6 +8,7 @@
 int a[N][N];
 
 int main() {
+
 #if defined(_OPENMP)
     printf("Hello, OpenMP v.%d!\n", _OPENMP);
 #else
@@ -17,55 +18,90 @@ int main() {
 
     for (int num_threads = 1; num_threads < 13; num_threads++) {
         omp_set_num_threads(num_threads);
-        printf("=============%d threads=============\n", num_threads);
+        printf("%d threads\n", num_threads);
 
         double start, end, t;
 
-        //заполнение нулями по строкам
+        // заполнение нулями по строкам
         start = omp_get_wtime();
-#pragma omp parallel for schedule(static)
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                a[i][j] = 0;
+        #pragma omp parallel
+        {
+            int tid = omp_get_thread_num();
+            int total_threads = omp_get_num_threads();
+
+            int rows_per_thread = N / total_threads;
+            int start_row = tid * rows_per_thread;
+            int end_row = (tid == total_threads - 1) ? N : start_row + rows_per_thread;
+
+            for (int i = start_row; i < end_row; i++) {
+                for (int j = 0; j < N; j++) {
+                    a[i][j] = 0;
+                }
             }
         }
         end = omp_get_wtime();
         t = end - start;
         printf("null time (raw): %.2lg seconds\n", t);
 
-        //заполнение нулями по столбцам
+        // заполнение нулями по столбцам
         start = omp_get_wtime();
-#pragma omp parallel for schedule(static)
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                a[j][i] = 0;
+        #pragma omp parallel
+        {
+            int tid = omp_get_thread_num();
+            int total_threads = omp_get_num_threads();
+
+            int columns_per_thread = N / total_threads;
+            int start_column = tid * columns_per_thread;
+            int end_column = (tid == total_threads - 1) ? N : start_column + columns_per_thread;
+
+            for (int i = start_column; i < end_column; i++) {
+                for (int j = 0; j < N; j++) {
+                    a[j][i] = 0;
+                }
             }
         }
         end = omp_get_wtime();
         t = end - start;
         printf("null time (column): %.2lg seconds\n", t);
 
-        //заполнение по возрастанию по строкам
+        // заполнение по возрастанию по строкам
         start = omp_get_wtime();
-#pragma omp parallel for schedule(static)
-        for (int i = 0; i < N; i++) {
-            int k = i * N;
-            for (int j = 0; j < N; j++) {
-                a[i][j] = k++;
+        #pragma omp parallel
+        {
+            int tid = omp_get_thread_num();
+            int total_threads = omp_get_num_threads();
+
+            int rows_per_thread = N / total_threads;
+            int start_row = tid * rows_per_thread;
+            int end_row = (tid == total_threads - 1) ? N : start_row + rows_per_thread;
+
+            int k = start_row * N;
+            for (int i = start_row; i < end_row; i++) {
+                for (int j = 0; j < N; j++) {
+                    a[i][j] = k++;
+                }
             }
         }
         end = omp_get_wtime();
         t = end - start;
         printf("sequence time (raw): %.2lg seconds\n", t);
 
-        //заполнение по возрастанию по столбцам
+        // заполнение по возрастанию по столбцам
         start = omp_get_wtime();
-#pragma omp parallel for schedule(static)
-        for (int i = 0; i < N; i++) {
-            int k = i;
-            for (int j = 0; j < N; j++) {
-                a[j][i] = k;
-                k += N;
+        #pragma omp parallel
+        {
+            int tid = omp_get_thread_num();
+            int total_threads = omp_get_num_threads();
+
+            int columns_per_thread = N / total_threads;
+            int start_column = tid * columns_per_thread;
+            int end_column = (tid == total_threads - 1) ? N : start_column + columns_per_thread;
+
+            int k = start_column * N;
+            for (int i = start_column; i < end_column; i++) {
+                for (int j = 0; j < N; j++) {
+                    a[j][i] = k++;
+                }
             }
         }
         end = omp_get_wtime();
